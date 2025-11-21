a569b1c3ea6724ee2c3f9316d93c0f9b2410ea8b
Silvia <163291053+Silvichka@users.noreply.github.com>
2025-11-21 15:37:31 +0100

    promotion plan and order classes are implemented

---
+++ src/main/java/com/group3/keo/Promotion/PromotionOrder.java
@@ -0,0 +1,73 @@
+package com.group3.keo.Promotion;
+
+import com.group3.keo.Enums.PromotionStatus;
+
+import java.time.LocalDateTime;
+
+public class PromotionOrder {
+
+    private final PromotionPlan plan;
+    private final Integer customViews;
+
+    private final double pricePerView;
+
+    private final LocalDateTime creationDateTime;
+    private PromotionStatus status;
+
+    public PromotionOrder(PromotionPlan plan,
+                          Integer customViews,
+                          double pricePerView) {
+
+        if (plan == null) {
+            throw new IllegalArgumentException("plan cannot be null");
+        }
+        if (pricePerView < 0) {
+            throw new IllegalArgumentException("pricePerView cannot be negative");
+        }
+
+        plan.validateCustomViews(customViews);
+
+        this.plan = plan;
+        this.customViews = customViews;
+        this.pricePerView = pricePerView;
+        this.creationDateTime = LocalDateTime.now();
+        this.status = PromotionStatus.IN_PROGRESS;
+    }
+
+    // ---------- getters ----------
+
+    public PromotionPlan getPlan() {
+        return plan;
+    }
+
+    public Integer getCustomViews() {
+        return customViews;
+    }
+
+    public double getPricePerView() {
+        return pricePerView;
+    }
+
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public PromotionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PromotionStatus status) {
+        if (status == null) {
+            throw new IllegalArgumentException("status cannot be null");
+        }
+        this.status = status;
+    }
+
+    public int getViews() {
+        return (customViews != null) ? customViews : plan.getViews();
+    }
+
+    public double getTotalPrice() {
+        return getViews() * pricePerView;
+    }
+}
