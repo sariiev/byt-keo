055bad64f2dafa8a41b69865632b5c2883a47a8a
sariiev <s30244@pjwstk.edu.pl>
2025-11-23 21:22:05 +0100

    add hasAudio and isAnimated fields to MediaAttachmentDTO

--- src/main/java/com/group3/keo/MediaAttachments/MediaAttachmentDTO.java
+++ src/main/java/com/group3/keo/MediaAttachments/MediaAttachmentDTO.java
@@ -1,4 +1,21 @@
 package com.group3.keo.MediaAttachments;
 
+import java.util.UUID;
+
 public class MediaAttachmentDTO {
+    public UUID uid;
+    public String type;
+
+    public String source;
+    public int fileSize;
+
+    Integer width;
+    Integer height;
+
+    Integer duration;
+    Integer channels;
+
+    Boolean hasAudio;
+
+    Boolean isAnimated;
 }
