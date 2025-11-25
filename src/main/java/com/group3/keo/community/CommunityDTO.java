839802d6ff5377bcc36a1f0208eef2517ae59355
sariiev <s30244@pjwstk.edu.pl>
2025-11-25 20:14:09 +0100

    add CommunityDTO class

---
+++ src/main/java/com/group3/keo/community/CommunityDTO.java
@@ -0,0 +1,12 @@
+package com.group3.keo.community;
+
+import com.group3.keo.enums.CommunityTopic;
+
+import java.util.UUID;
+
+public class CommunityDTO {
+    UUID uid;
+    String name;
+    CommunityTopic topic;
+    UUID avatarUid;
+}
