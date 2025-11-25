0e97031e7ba6cff4637d76c963c3f1498b366d6b
sariiev <s30244@pjwstk.edu.pl>
2025-11-25 21:06:35 +0100

    add RoleDTO class

---
+++ src/main/java/com/group3/keo/community/RoleDTO.java
@@ -0,0 +1,11 @@
+package com.group3.keo.community;
+
+import com.group3.keo.enums.RoleType;
+
+import java.util.UUID;
+
+public class RoleDTO {
+    public UUID communityUid;
+    public UUID userUid;
+    public RoleType roleType;
+}
