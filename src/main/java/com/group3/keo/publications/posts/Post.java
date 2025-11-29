8b0278e2f573380efeac7d7a9cafb00f1037d4cd
sariiev <s30244@pjwstk.edu.pl>
2025-11-29 08:12:19 +0100

    reoganize package structure, add constructor with uid parameter

--- src/main/java/com/group3/keo/publications/posts/Post.java
+++ src/main/java/com/group3/keo/publications/posts/Post.java
@@ -1,24 +1,34 @@
-package com.group3.keo.publications;
+package com.group3.keo.publications.posts;
 
 import com.group3.keo.media.MediaAttachment;
+import com.group3.keo.publications.base.PublicationAuthor;
+import com.group3.keo.publications.base.PublicationBase;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
-public class Post extends PublicationBase{
+public abstract class Post extends PublicationBase {
 
-    private boolean wasPromoted;
+    protected boolean wasPromoted = false;
 
-    public Post(String caption, LocalDateTime publicationDateTime, List<MediaAttachment> attachments) {
-        super(caption, publicationDateTime, attachments);
+    protected Post(PublicationAuthor author, String caption, List<MediaAttachment> attachments) {
+        super(author, caption, attachments);
     }
 
-    public boolean isWasPromoted() {
+    protected Post(UUID uid, PublicationAuthor author, String caption, List<MediaAttachment> attachments, LocalDateTime publicationDateTime, int views, boolean wasEdited, boolean wasPromoted) {
+        super(uid, author, caption, attachments, publicationDateTime, views, wasEdited);
+        this.wasPromoted = wasPromoted;
+    }
+
+    public boolean wasPromoted() {
         return wasPromoted;
     }
 
     public void setWasPromoted(boolean wasPromoted) {
+        if (this.wasPromoted && !wasPromoted) {
+            throw new IllegalStateException("wasPromoted can't be changed from true to false");
+        }
         this.wasPromoted = wasPromoted;
     }
-
 }
