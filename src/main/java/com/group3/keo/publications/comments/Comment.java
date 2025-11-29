86c1a199a102c7acb735152dca02319df39b7842
sariiev <s30244@pjwstk.edu.pl>
2025-11-29 08:13:33 +0100

    reorganize package structure, add constructor with uid parameter, add canView(user) method

--- src/main/java/com/group3/keo/publications/comments/Comment.java
+++ src/main/java/com/group3/keo/publications/comments/Comment.java
@@ -1,28 +1,56 @@
-package com.group3.keo.publications;
+package com.group3.keo.publications.comments;
 
 import com.group3.keo.media.MediaAttachment;
+import com.group3.keo.publications.base.PrivatePublication;
+import com.group3.keo.publications.base.PublicationAuthor;
 import com.group3.keo.publications.base.PublicationBase;
+import com.group3.keo.users.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 public class Comment extends PublicationBase {
 
     private final PublicationBase commentedPublication;
 
-    public Comment(String caption,
-                   LocalDateTime publicationDateTime,
+    public Comment(PublicationAuthor author,
+                   String caption,
                    PublicationBase commentedPublication,
                    List<MediaAttachment> attachments) {
-        super(caption, publicationDateTime, attachments);
+        super(author, caption, attachments);
         if (commentedPublication == null) {
             throw new IllegalArgumentException("publication cannot be null");
         }
         this.commentedPublication = commentedPublication;
+        commentedPublication.addComment(this);
+    }
+
+    public Comment(UUID uid,
+                   PublicationAuthor author,
+                   String caption,
+                   PublicationBase commentedPublication,
+                   List<MediaAttachment> attachments,
+                   LocalDateTime publicationDateTime,
+                   int views,
+                   boolean wasEdited) {
+        super(uid, author, caption, attachments, publicationDateTime, views, wasEdited);
+        if (commentedPublication == null) {
+            throw new IllegalArgumentException("publication cannot be null");
+        }
+        this.commentedPublication = commentedPublication;
+        commentedPublication.addComment(this);
     }
 
     public PublicationBase getCommentedPublication() {
         return commentedPublication;
     }
 
+    public boolean canView(User user) {
+        if (commentedPublication instanceof PrivatePublication privatePublication) {
+            return privatePublication.canView(user);
+        }
+
+        return true;
+    }
 }
