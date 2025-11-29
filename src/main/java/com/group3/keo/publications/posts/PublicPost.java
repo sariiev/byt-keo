0dc5e9fe472196802472ae8c34a0b8846cb03427
sariiev <s30244@pjwstk.edu.pl>
2025-11-29 08:12:38 +0100

    add PublicPost class

--- src/main/java/com/group3/keo/publications/posts/PublicPost.java
+++ src/main/java/com/group3/keo/publications/posts/PublicPost.java
@@ -1,4 +1,19 @@
 package com.group3.keo.publications.posts;
 
-public class PublicPost {
+import com.group3.keo.media.MediaAttachment;
+import com.group3.keo.publications.base.PublicPublication;
+import com.group3.keo.publications.base.PublicationAuthor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class PublicPost extends Post implements PublicPublication {
+    public PublicPost(PublicationAuthor author, String caption, List<MediaAttachment> attachments) {
+        super(author, caption, attachments);
+    }
+
+    public PublicPost(UUID uid, PublicationAuthor author, String caption, List<MediaAttachment> attachments, LocalDateTime publicationDateTime, int views, boolean wasEdited, boolean wasPromoted) {
+        super(uid, author, caption, attachments, publicationDateTime, views, wasEdited, wasPromoted);
+    }
 }
